197d54cdc66a7811131c14333f9eb95b49f07f3c
Shelbyrp <77222563+Shelbyrp@user.noreply.github.com>
2021-08-06 06:26:46 +0800

    Change nav colour.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,60 +6,63 @@ import Typography from '@material-ui/core/Typography';
 import { MenuItem } from '@material-ui/core';
 import logo from '../img/logo.png';
 
-
 const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
-  appBar: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-  logo: {
-    maxWidth: 160,
-  },
+    appBar: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+    logo: {
+        maxWidth: 160,
+    },
+    MuiAppBar: {
+        color: '#fff',
+        background:'#03313d',
+},
 }));
 
 function Navbar() {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <AppBar>
-      <Toolbar>
-         <img src={logo} alt="logo" className={classes.logo} />
-        <MenuItem>
-          <Typography variant='h6' className={classes.title}>
-            Home
-          </Typography>
-        </MenuItem>
-        <MenuItem>
-          <Typography variant='h6' className={classes.title}>
-            About
-          </Typography>
-        </MenuItem>
-        <MenuItem>
-          <Typography variant='h6' className={classes.title}>
-            Map
-          </Typography>
-                  </MenuItem>
-                  <MenuItem>
-          <Typography variant='h6' className={classes.title}>
-            Journal
-          </Typography>
-          </MenuItem>
-          <MenuItem>
-          <Typography variant='h6' className={classes.title}>
-            Play
-          </Typography>
-        </MenuItem>
-      </Toolbar>
-    </AppBar>
-  );
+    return (
+        <AppBar className={classes.MuiAppBar}>
+            <Toolbar>
+                <img src={logo} alt="logo" className={classes.logo} />
+                <MenuItem>
+                    <Typography variant='h6' className={classes.title}>
+                        Home
+                    </Typography>
+                </MenuItem>
+                <MenuItem>
+                    <Typography variant='h6' className={classes.title}>
+                        About
+                    </Typography>
+                </MenuItem>
+                <MenuItem>
+                    <Typography variant='h6' className={classes.title}>
+                        Map
+                    </Typography>
+                </MenuItem>
+                <MenuItem>
+                    <Typography variant='h6' className={classes.title}>
+                        Journal
+                    </Typography>
+                </MenuItem>
+                <MenuItem>
+                    <Typography variant='h6' className={classes.title}>
+                        Play
+                    </Typography>
+                </MenuItem>
+            </Toolbar>
+        </AppBar>
+    );
 }
 
 export default Navbar;
