4ad7931f833f8e19392cef686f3f96e75230d268
Shelbyrp <77222563+Shelbyrp@user.noreply.github.com>
2021-08-06 06:50:32 +0800

    Add hero image and h1 text.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,30 +1,45 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Navbar from '../components/Navbar';
-import hero from '../img/logo.png'
+import hero from '../img/hero.png'
 import { Container } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
     root: {
-      display: 'flex',
+        display: 'flex',
     },
     Media: {
         height: '100%',
-        width: '100%'
-      }
-  }));
-  
-  function Home() {
+        width: '100%',
+        zIndex: -10,
+    },
+    Container: {
+        position: 'relative',
+        height: '100vh',
+        width: '100%',
+        padding: '0px',
+    },
+    heading: {
+        position: 'absolute',
+        top: '40%',
+        left: '20%',
+        fontSize: '6rem',
+        color: '#fff'
+    }
+}));
+
+function Home() {
     const classes = useStyles();
-  
+
     return (
-      <div className={classes.root}>
-        <Navbar />
-        <Container>
-        <img src={hero} alt="hero" className={classes.Media} />
-        </Container>
-      </div>
+        <div className={classes.root}>
+            <Navbar />
+            <Container disableGutters maxWidth={false} className={classes.Container}>
+                <img src={hero} alt="hero" className={classes.Media} />
+                <h1 className={classes.heading}>Adventures Worth Noting</h1>
+            </Container>
+        </div>
     );
-  }
-  
-  export default Home;
+}
+
+export default Home;
