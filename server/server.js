6f910915f54794d81c27258787405f8ffdabbd6c
Shelbyrp <77222563+Shelbyrp@user.noreply.github.com>
2021-08-05 21:25:31 +0800

    update package.json

--- server/server.js
+++ server/server.js
@@ -1,34 +1,35 @@
-require("dotenv").config();
-const express = require("express");
-const { ApolloServer } = require("apollo-server-express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+//const routes = require('./routes');
 
-const { typeDefs, resolvers } = require("./schemas");
-const { authMiddleware } = require("./utils/auth");
-const db = require("./config/connection");
-
-const PORT = process.env.PORT || 3001;
 const app = express();
-const apolloServer = new ApolloServer({
+const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
     typeDefs,
     resolvers,
     context: authMiddleware,
 });
 
-apolloServer.applyMiddleware({ app });
+server.applyMiddleware({ app })
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/build")));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-db.once("open", () => {
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+db.once('open', () => {
     app.listen(PORT, () => {
         console.log(`API server running on port ${PORT}!`);
-        console.log(
-            `Use GraphQL at http://localhost:${PORT}${apolloServer.graphqlPath}`
-        );
+        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     });
 });
