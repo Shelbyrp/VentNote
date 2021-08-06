a6a439806618a613676603c7ec63a304c64619b1
Shelbyrp <77222563+Shelbyrp@user.noreply.github.com>
2021-08-06 20:18:51 +0800

    Add to the journal, map and play pages.

---
+++ client/src/pages/Journal.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import hero from '../img/VNHero.png'
+import { Container } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+    },
+    Media: {
+        height: '100%',
+        width: '100%',
+        zIndex: -10,
+    },
+    Container: {
+        position: 'relative',
+        height: '100vh',
+        width: '100%',
+        padding: '0px',
+    },
+    heading: {
+        position: 'absolute',
+        top: '40%',
+        left: '20%',
+        fontSize: '6rem',
+        color: '#fff'
+    }
+}));
+
+function Journal() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+           
+            <Container disableGutters maxWidth={false} className={classes.Container}>
+                <img src={hero} alt="hero" className={classes.Media} />
+                <h1 className={classes.heading}>journal</h1>
+            </Container>
+        </div>
+    );
+}
+
+export default Journal;
