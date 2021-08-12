e75538aa63fd4ac47f8da0818f95a5b4d946428a
Shelbyrp <77222563+Shelbyrp@user.noreply.github.com>
2021-08-12 18:39:20 +0800

    Styling changes to journal form.

--- client/src/components/JournalForm/index.js
+++ client/src/components/JournalForm/index.js
@@ -3,20 +3,29 @@ import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 import { makeStyles } from '@material-ui/core/styles';
-
+import Container from '@material-ui/core/Container';
 import { ADD_JOURNAL } from '../../utils/mutations';
 import { QUERY_JOURNALS, QUERY_ME } from '../../utils/queries';
-
+import { TextField } from '@material-ui/core';
 import Auth from '../../utils/auth';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    errorMessage: {
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center'
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: theme.spacing(2),
+    '& .MuiTextField-root': {
+        margin: theme.spacing(1),
+        width: '500px',
+    },
+    '& .MuiButtonBase-root': {
+        margin: theme.spacing(2),
     }
+  },
+  text: {
   }
 }));
 
@@ -96,62 +105,63 @@ const JournalForm = () => {
 
   return (
     <div>
-      <h3>What latest experience do you want to record?</h3>
+      <Container maxWidth="lg" className={classes.blogsContainer}>
+        <h3>What latest experience do you want to record?</h3>
 
-      {Auth.loggedIn() ? (
-        <>
-          <p
-            className={`m-0 ${characterCount === 500 || error ? 'text-danger' : ''
-              }`}
-          >
-            Character Count: {characterCount}/500
-          </p>
-          <form
-            className={classes.root} onSubmit={handleFormSubmit}
-          >
-            <TextareaAutosize
-              name="journalTitle"
-              placeholder="Journal Title"
-              value={journalTitle}
-              className="form-input w-200"
-              style={{ lineHeight: '1.5', resize: 'vertical' }}
-              onChange={handleChange}
-            ></TextareaAutosize>
-            <TextareaAutosize
-              name="journalAddress"
-              placeholder="Journal Address"
-              value={journalAddress}
-              className="form-input w-200"
-              style={{ lineHeight: '1.5', resize: 'vertical' }}
-              onChange={handleChange}
-            ></TextareaAutosize>
-            <TextareaAutosize
-              name="journalText"
-              placeholder="Here's a new journal..."
-              value={journalText}
-              required
-              className="form-input w-500"
-              style={{ lineHeight: '1.5', resize: 'vertical' }}
-              onChange={handleChange}
-            ></TextareaAutosize>
-            <div className="col-12 col-lg-3">
-              <button className="btn btn-primary btn-block py-3" type="submit">
-                Add Journal
-              </button>
-            </div>
-            {/* {error && (
+        {Auth.loggedIn() ? (
+          <>
+            <p
+              className={`m-0 ${characterCount === 500 || error ? 'text-danger' : ''
+                }`}
+            >
+              Character Count: {characterCount}/500
+            </p>
+            <form
+              className={classes.root} onSubmit={handleFormSubmit}
+            >
+              <TextField
+                name="journalTitle"
+                placeholder="Journal Title"
+                value={journalTitle}
+                style={{ lineHeight: '1.5', resize: 'vertical' }}
+                onChange={handleChange}
+              ></TextField>
+              <TextField
+                name="journalAddress"
+                placeholder="Journal Address"
+                value={journalAddress}
+                style={{ lineHeight: '1.5', resize: 'vertical' }}
+                onChange={handleChange}
+              ></TextField>
+              <TextareaAutosize
+                name="journalText"
+                placeholder="Here's a new journal..."
+                value={journalText}
+                required
+                className="text"
+                style={{ lineHeight: '1.5', width: '500px', height: "200px", resize: 'vertical', marginTop: '10px' }}
+                onChange={handleChange}
+              ></TextareaAutosize>
+              <div>
+                <Button variant="contained" color="primary"
+                  style={{ cursor: 'pointer', marginTop: '10px' }} type="submit">
+                  Add Journal
+                </Button>
+              </div>
+              {/* {error && (
               <div className="col-12 my-3 bg-danger text-white p-3">
                 {error.message}
               </div>
             )} */}
-          </form>
-        </>
-      ) : (
-        <p>
-          You need to be logged in to share your journals. Please{' '}
-          <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
-        </p>
-      )}
+            </form>
+          </>
+        ) : (
+          <p>
+            You need to be logged in to share your journals. Please{' '}
+            <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
+          </p>
+        )}
+      </Container>
     </div>
   );
 };
